bc5ba6f1d6e82c3bea27f8f10c771f88a5713544
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-08-05 13:42:09 -0500

    Two-way-databinding example is implemented

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,7 +9,7 @@ export class ServersComponent implements OnInit {
 
   allowNewServer = false;
   serverWasCreated = "Server was not created!";
-  serverName = '';
+  serverName = 'server test';
 
   constructor() {
     setTimeout(() => {
