a9e7eb0004924816e65394b5f566aa4d0e3e9598
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-09-23 19:39:33 -0500

    BasicDirective is created - Custom directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { ServersComponent } from './servers/servers.component';
 import { FormsModule } from '@angular/forms';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
+import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { ServerElementComponent } from './server-element/server-element.componen
     ServerComponent,
     ServersComponent,
     CockpitComponent,
-    ServerElementComponent
+    ServerElementComponent,
+    BasicHighlightDirective
   ],
   imports: [
     BrowserModule,
