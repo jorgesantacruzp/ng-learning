ac447c283fa59fd256fb8d17b863a086564a008b
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-09-26 21:57:49 -0500

    Class refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
+import {ServerComponent} from "./unused/server/server.component";
 import { ServersComponent } from './servers/servers.component';
 
 import { FormsModule } from '@angular/forms';
 import { CockpitComponent } from './cockpit/cockpit.component';
 import { ServerElementComponent } from './server-element/server-element.component';
-import { BasicHighlightDirective } from "./basic-highlight/basic-highlight.directive";
-import { BetterHighlightDirective } from './better-highlight/better-highlight.directive';
-import { UnlessDirective } from './unless/unless.directive';
+import { BasicHighlightDirective } from "./directives/attribute/basic-highlight/basic-highlight.directive";
+import { BetterHighlightDirective } from './directives/attribute/better-highlight/better-highlight.directive';
+import { UnlessDirective } from './directives/structural/unless/unless.directive';
 
 @NgModule({
   declarations: [
