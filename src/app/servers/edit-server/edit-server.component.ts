cb70f5cfaa803d9f107c9c4dac4aa9e57d6d2d0d
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-11-04 10:46:55 -0500

    EditServerComponent use an observable in order to keep server updated

--- src/app/servers/edit-server/edit-server.component.ts
+++ src/app/servers/edit-server/edit-server.component.ts
@@ -25,7 +25,14 @@ export class EditServerComponent implements OnInit {
           this.allowEdit = queryParams['allowEdit'] === '1' ? true : false;
         }
       );
-    this.server = this.serversService.getServer(1);
+    const id = this.route.snapshot.params['id'];
+    this.server = this.serversService.getServer(+id);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
