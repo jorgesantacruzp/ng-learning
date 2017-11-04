ef2d88c46f2b19dca3f9cb53127c267da3f87f19
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-11-04 09:51:17 -0500

    Child nested routes are set up

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Params} from "@angular/router";
 
 @Component({
   selector: 'app-server',
@@ -17,6 +17,12 @@ export class ServerComponent implements OnInit {
   ngOnInit() {
     const id = Number(this.route.snapshot.params['id']);
     this.server = this.serversService.getServer(id);
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.server = this.serversService.getServer(+params['id']);
+        }
+      );
   }
 
 }
