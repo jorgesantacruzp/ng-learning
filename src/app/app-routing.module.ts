927e52e8f985bf6fa21ac048fece72faed6bad89
Jorge Santa Cruz <jorge.santacruzp@gmail.com>
2017-11-06 21:51:23 -0500

    Resolver is used to load data before rendering a route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,10 @@ import {UserComponent} from "./users/user/user.component";
 import {ServersComponent} from "./servers/servers.component";
 import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuard} from "./auth-guard.service";
 import {CanDeactivateGuard} from "./servers/edit-server/can-deactivate-guard.service";
 import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolver} from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -18,7 +18,7 @@ const appRoutes: Routes = [
     { path: ':id/:name', component: UserComponent }
   ] },
   { path: 'servers', canActivateChild: [AuthGuard], component: ServersComponent, children: [
-    { path: ':id', component: ServerComponent },
+    { path: ':id', component: ServerComponent, resolve: {server: ServerResolver} },
     { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard] }
   ]},
   //{ path: 'not-found', component: PageNotFoundComponent },
